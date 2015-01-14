7658147c63d446c9aa2924857d95ca32ffd909fe
OneKeyToWin <kaczor.sebastian@gmail.cm>
2015-01-14 17:48:21 +0100

    1.7.7
    1.7.7

--- Jinx/Program.cs
+++ Jinx/Program.cs
@@ -201,8 +201,6 @@ namespace Jinx
                     {
                         float predictedHealth = HealthPrediction.GetHealthPrediction(target, (int)(R.Delay + (Player.Distance(target.ServerPosition) / R.Speed) * 1000));
                         var Rdmg = R.GetDamage(target);
-                        if (ObjectManager.Player.Health < ObjectManager.Player.MaxHealth * 0.4)
-                            Rdmg = R.GetDamage(target) * 1.4f;
                         if (Rdmg > predictedHealth && GetRealDistance(target) > bonusRange() + 150 + target.BoundingRadius && CountAlliesNearTarget(target, 500) == 0 && CountEnemies(ObjectManager.Player, GetRealPowPowRange(target)) == 0)
                         {
                             cast = true;
