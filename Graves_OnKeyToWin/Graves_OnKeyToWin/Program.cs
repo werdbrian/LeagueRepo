fed15692856021b756b7e093e09b5c57b5b9e751
OneKeyToWin <kaczor.sebastian@gmail.cm>
2015-01-25 14:55:48 +0100

    Graves beta

---
+++ Graves_OnKeyToWin/Graves_OnKeyToWin/Program.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.IO;
+using SharpDX;
+using Collision = LeagueSharp.Common.Collision;
+namespace Graves_OnKeyToWin
+{
+    class Program
+    {
+        public const string ChampionName = "Jinx";
+
+        //Orbwalker instance
+        public static Orbwalking.Orbwalker Orbwalker;
+        //Spells
+        public static List<Spell> SpellList = new List<Spell>();
+        public static Spell Q;
+        public static Spell Q1;
+        public static Spell W;
+        public static Spell E;
+        public static Spell R;
+        public static Spell R1;
+        //ManaMenager
+        public static float QMANA;
+        public static float WMANA;
+        public static float EMANA;
+        public static float RMANA;
+        public static bool Farm = false;
+        public static double WCastTime = 0;
+        //AutoPotion
+        public static Items.Item Potion = new Items.Item(2003, 0);
+        public static Items.Item ManaPotion = new Items.Item(2004, 0);
+        public static Items.Item Youmuu = new Items.Item(3142, 0);
+
+        //Menu
+        public static Menu Config;
+
+        private static Obj_AI_Hero Player;
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Player = ObjectManager.Player;
+            if (Player.BaseSkinName != ChampionName) return;
+
+            //Create the spells
+            Q = new Spell(SpellSlot.Q, 900f);
+            Q1 = new Spell(SpellSlot.Q, 950f);
+            W = new Spell(SpellSlot.W, 1500f);
+            E = new Spell(SpellSlot.E, 450f);
+            R = new Spell(SpellSlot.R, 1000f);
+            R1 = new Spell(SpellSlot.R, 1800f);
+
+            Q.SetSkillshot(0.26f, 10f * 2 * (float)Math.PI / 180, 1950f, false, SkillshotType.SkillshotCone);
+            Q1.SetSkillshot(0.26f, 50f, 1950f, false, SkillshotType.SkillshotLine);
+            W.SetSkillshot(0.35f, 250f, 1650f, false, SkillshotType.SkillshotCircle);
+
+            R.SetSkillshot(0.25f, 100f, 2100f, true, SkillshotType.SkillshotLine);
+            R1.SetSkillshot(0.25f, 10f * (float)Math.PI / 180, 1950f, false, SkillshotType.SkillshotCone);
+
+            SpellList.Add(Q);
+            SpellList.Add(Q1);
+            SpellList.Add(W);
+            SpellList.Add(E);
+            SpellList.Add(R);
+            SpellList.Add(R1);
+            //Create the menu
+            Config = new Menu(ChampionName, ChampionName, true);
+
+            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            TargetSelector.AddToMenu(targetSelectorMenu);
+            Config.AddSubMenu(targetSelectorMenu);
+
+            //Orbwalker submenu
+            Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+
+            //Load the orbwalker and add it to the submenu.
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+            Config.AddToMainMenu();
+            Config.AddItem(new MenuItem("noti", "Show notification").SetValue(true));
+            Config.AddItem(new MenuItem("pots", "Use pots").SetValue(true));
+            Config.AddItem(new MenuItem("opsE", "OnProcessSpellCastE").SetValue(true));
+            Config.AddItem(new MenuItem("hitchanceR", "VeryHighHitChanceR").SetValue(true));
+            Config.AddItem(new MenuItem("autoR", "Auto R").SetValue(true));
+            Config.AddItem(new MenuItem("useR", "Semi-manual cast R key").SetValue(new KeyBind('t', KeyBindType.Press))); //32 == space
+            //Add the events we are going to use:
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Orbwalking.BeforeAttack += BeforeAttack;
+            Orbwalking.AfterAttack += afterAttack;
+            Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
+            Game.PrintChat("<font color=\"#ff00d8\">J</font>inx full automatic SI ver 1.9 <font color=\"#000000\">by sebastiank1</font> - <font color=\"#00BFFF\">Loaded</font>");
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            ManaMenager();
+            if (Orbwalker.ActiveMode.ToString() == "Mixed" || Orbwalker.ActiveMode.ToString() == "LaneClear" || Orbwalker.ActiveMode.ToString() == "LastHit")
+                Farm = true;
+            else
+                Farm = false;
+
+            if ( W.IsReady())
+            {
+                var t = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Magical);
+                if (t.IsValidTarget())
+                {
+                    if (W.GetDamage(t) > t.Health)
+                        W.Cast(t, true, true);
+                    else if (Orbwalker.ActiveMode.ToString() == "Combo" && ObjectManager.Player.Mana > RMANA + QMANA + EMANA + WMANA)
+                        W.Cast(t, true, true);
+                }
+            }
+
+            if (E.IsReady())
+            {
+                ManaMenager();
+                var t = TargetSelector.GetTarget(E.Range + Q.Range, TargetSelector.DamageType.Physical);
+                var t2 = TargetSelector.GetTarget(1000, TargetSelector.DamageType.Physical);
+
+                if (Orbwalker.ActiveMode.ToString() == "Combo" && ObjectManager.Player.Mana > RMANA + EMANA && GetRealDistance(t) < 400 && ObjectManager.Player.Health < ObjectManager.Player.MaxHealth * 0.8)
+                    E.Cast(Game.CursorPos, true);
+                else if (Orbwalker.ActiveMode.ToString() == "Combo"
+                && t.IsValidTarget()
+                && ObjectManager.Player.Health > ObjectManager.Player.MaxHealth * 0.3
+                && Q.IsReady()
+                && ObjectManager.Player.Mana > QMANA + EMANA
+                && Q.GetDamage(t) > t.Health
+                && GetRealDistance(t) > GetRealRange(t)
+                && !ObjectManager.Player.UnderTurret(true)
+                && t.CountEnemiesInRange(800) < 3)
+                {
+                    E.Cast(t.ServerPosition, true);
+                }
+                else if (Orbwalker.ActiveMode.ToString() == "Combo"
+                && !Q.IsReady()
+                && t2.IsValidTarget()
+                && ObjectManager.Player.Health > ObjectManager.Player.MaxHealth * 0.3
+                && ObjectManager.Player.Mana > RMANA + EMANA
+                && (ObjectManager.Player.GetAutoAttackDamage(t2) * 2 > t.Health)
+                && GetRealDistance(t2) > GetRealRange(t2) + 100
+                && !ObjectManager.Player.UnderTurret(true)
+                && t2.CountEnemiesInRange(800) < 3)
+                {
+                    E.Cast(t2.ServerPosition, true);
+                }
+                
+                if (Config.Item("useE").GetValue<KeyBind>().Active)
+                    E.Cast(Game.CursorPos, true);
+
+            }
+
+            if (Q.IsReady())
+            {
+                ManaMenager();
+                var t = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical);
+                if (t.IsValidTarget())
+                {
+                    var qDmg = Q.GetDamage(t);
+                    if (GetRealDistance(t) > bonusRange() + 60 && qDmg > t.Health)
+                        Q.Cast(t, true);
+                    else if (Orbwalker.ActiveMode.ToString() == "Combo" && ObjectManager.Player.Mana > RMANA + QMANA + EMANA && Orbwalker.GetTarget() == null)
+                        Q.CastIfHitchanceEquals(t, HitChance.High, true);
+                    else if ((Farm && ObjectManager.Player.Mana > RMANA + EMANA + QMANA + QMANA) && Orbwalker.GetTarget() == null)
+                    {
+                        if (ObjectManager.Player.Mana > ObjectManager.Player.MaxMana * 0.9)
+                            Q.CastIfHitchanceEquals(t, HitChance.VeryHigh, true);
+                        else if (t.Path.Count() > 1)
+                            Q1.CastIfHitchanceEquals(t, HitChance.VeryHigh, true);
+                    }
+                    else if ((Orbwalker.ActiveMode.ToString() == "Combo" || Farm) && ObjectManager.Player.Mana > RMANA + QMANA && ObjectManager.Player.CountEnemiesInRange(GetRealRange(t) - 100) == 0)
+                    {
+                        foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(enemy => enemy.IsValidTarget(Q.Range)))
+                        {
+                            if (enemy.HasBuffOfType(BuffType.Stun) || enemy.HasBuffOfType(BuffType.Snare) ||
+                             enemy.HasBuffOfType(BuffType.Charm) || enemy.HasBuffOfType(BuffType.Fear) ||
+                             enemy.HasBuffOfType(BuffType.Taunt) || enemy.HasBuffOfType(BuffType.Slow) || enemy.HasBuff("Recall"))
+                                Q.CastIfHitchanceEquals(t, HitChance.High, true);
+                        }
+                    }
+                }
+            }
+
+            if (R.IsReady() && Config.Item("autoR").GetValue<bool>())
+            {
+                bool cast = false;
+                foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(target => target.IsValidTarget(R.Range)))
+                {
+                    if (target.IsValidTarget() && (Game.Time - WCastTime > 1) &&
+                        !target.HasBuffOfType(BuffType.PhysicalImmunity) && !target.HasBuffOfType(BuffType.SpellImmunity) && !target.HasBuffOfType(BuffType.SpellShield))
+                    {
+                        float predictedHealth = HealthPrediction.GetHealthPrediction(target, (int)(R.Delay + (Player.Distance(target.ServerPosition) / R.Speed) * 1000));
+                        var Rdmg = R.GetDamage(target);
+                        if (Rdmg > predictedHealth && Orbwalker.GetTarget() == null)
+                        {
+                            cast = true;
+                            PredictionOutput output = R.GetPrediction(target);
+                            Vector2 direction = output.CastPosition.To2D() - Player.Position.To2D();
+                            direction.Normalize();
+                            List<Obj_AI_Hero> enemies = ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsEnemy && x.IsValidTarget()).ToList();
+                            foreach (var enemy in enemies)
+                            {
+                                if (enemy.SkinName == target.SkinName || !cast)
+                                    continue;
+                                PredictionOutput prediction = R.GetPrediction(enemy);
+                                Vector3 predictedPosition = prediction.CastPosition;
+                                Vector3 v = output.CastPosition - Player.ServerPosition;
+                                Vector3 w = predictedPosition - Player.ServerPosition;
+                                double c1 = Vector3.Dot(w, v);
+                                double c2 = Vector3.Dot(v, v);
+                                double b = c1 / c2;
+                                Vector3 pb = Player.ServerPosition + ((float)b * v);
+                                float length = Vector3.Distance(predictedPosition, pb);
+                                if (length < (100 + enemy.BoundingRadius) && Player.Distance(predictedPosition) < Player.Distance(target.ServerPosition))
+                                    cast = false;
+                            }
+                            if (cast && target.IsValidTarget() )
+                                R.Cast(target, true);
+                        }
+                    }
+                }
+            }
+            PotionMenager();
+        }
+        private static void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser)
+        {
+            if (Config.Item("AGC").GetValue<bool>() && E.IsReady() && ObjectManager.Player.Mana > RMANA + EMANA)
+            {
+                var Target = (Obj_AI_Hero)gapcloser.Sender;
+                if (Target.IsValidTarget(E.Range))
+                    E.Cast(Game.CursorPos, true);
+                return;
+            }
+            return;
+        }
+
+        private static void afterAttack(AttackableUnit unit, AttackableUnit target)
+        {
+
+
+        }
+
+        static void BeforeAttack(Orbwalking.BeforeAttackEventArgs args)
+        {
+
+        }
+
+        public static void UseItem(int id, Obj_AI_Hero target = null)
+        {
+            if (Items.HasItem(id) && Items.CanUseItem(id))
+            {
+                Items.UseItem(id, target);
+            }
+        }
+
+        public static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base unit, GameObjectProcessSpellCastEventArgs args)
+        {
+            double ShouldUse = ShouldUseE(args.SData.Name);
+
+            if (Config.Item("opsE").GetValue<bool>() && unit.Team != ObjectManager.Player.Team && ShouldUse >= 0f && unit.IsValidTarget(E.Range))
+                E.Cast(unit.ServerPosition, true);
+            if (unit.IsMe && (args.SData.Name == "CaitlynPiltoverPeacemaker" || args.SData.Name == "CaitlynEntrapment"))
+            {
+                WCastTime = Game.Time;
+            }
+        }
+
+        public static double ShouldUseE(string SpellName)
+        {
+            if (SpellName == "ThreshQ")
+                return 0;
+            if (SpellName == "KatarinaR")
+                return 0;
+            if (SpellName == "AlZaharNetherGrasp")
+                return 0;
+            if (SpellName == "GalioIdolOfDurand")
+                return 0;
+            if (SpellName == "LuxMaliceCannon")
+                return 0;
+            if (SpellName == "MissFortuneBulletTime")
+                return 0;
+            if (SpellName == "RocketGrabMissile")
+                return 0;
+            if (SpellName == "CaitlynPiltoverPeacemaker")
+                return 0;
+            if (SpellName == "EzrealTrueshotBarrage")
+                return 0;
+            if (SpellName == "InfiniteDuress")
+                return 0;
+            if (SpellName == "VelkozR")
+                return 0;
+            return -1;
+        }
+
+        private static float GetRealRange(GameObject target)
+        {
+            return 600f + ObjectManager.Player.BoundingRadius + target.BoundingRadius;
+        }
+
+        public static float bonusRange()
+        {
+            return 600f + ObjectManager.Player.BoundingRadius;
+        }
+        private static float GetRealDistance(GameObject target)
+        {
+            return ObjectManager.Player.ServerPosition.Distance(target.Position) + ObjectManager.Player.BoundingRadius +
+                   target.BoundingRadius;
+        }
+
+        public static void ManaMenager()
+        {
+            QMANA = Q.Instance.ManaCost;
+            WMANA = W.Instance.ManaCost;
+            EMANA = E.Instance.ManaCost;
+            if (!R.IsReady())
+                RMANA = QMANA - ObjectManager.Player.Level * 2;
+            else
+                RMANA = R.Instance.ManaCost;
+
+            RMANA = RMANA + (ObjectManager.Player.CountEnemiesInRange(2500) * 20);
+
+            if (ObjectManager.Player.Health < ObjectManager.Player.MaxHealth * 0.2)
+            {
+                QMANA = 0;
+                WMANA = 0;
+                EMANA = 0;
+                RMANA = 0;
+            }
+        }
+
+        public static void PotionMenager()
+        {
+            if (Config.Item("pots").GetValue<bool>() && !ObjectManager.Player.InFountain() && !ObjectManager.Player.HasBuff("Recall"))
+            {
+                if (Potion.IsReady() && !ObjectManager.Player.HasBuff("RegenerationPotion", true))
+                {
+                    if (ObjectManager.Player.CountEnemiesInRange(700) > 0 && ObjectManager.Player.Health + 200 < ObjectManager.Player.MaxHealth)
+                        Potion.Cast();
+                    else if (ObjectManager.Player.Health < ObjectManager.Player.MaxHealth * 0.6)
+                        Potion.Cast();
+                }
+                if (ManaPotion.IsReady() && !ObjectManager.Player.HasBuff("FlaskOfCrystalWater", true))
+                {
+                    if (ObjectManager.Player.CountEnemiesInRange(1200) > 0 && ObjectManager.Player.Mana < RMANA + WMANA + EMANA)
+                        ManaPotion.Cast();
+                }
+            }
+        }
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (Config.Item("noti").GetValue<bool>())
+            {
+                var t = TargetSelector.GetTarget(500 * R.Level + 1500, TargetSelector.DamageType.Physical);
+                float predictedHealth = HealthPrediction.GetHealthPrediction(t, (int)(R.Delay + (Player.Distance(t.ServerPosition) / R.Speed) * 1000));
+                if (t.IsValidTarget() && R.IsReady())
+                {
+                    var rDamage = R.GetDamage(t);
+                    if (rDamage > predictedHealth)
+                        Drawing.DrawText(Drawing.Width * 0.1f, Drawing.Height * 0.5f, System.Drawing.Color.Red, "Ult can kill: " + t.ChampionName + " have: " + t.Health + "hp");
+                    if (Config.Item("useR").GetValue<KeyBind>().Active)
+                    {
+                        R.Cast(t, true);
+                    }
+                }
+                var tw = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Physical);
+                if (tw.IsValidTarget())
+                {
+                    var qDmg = W.GetDamage(tw);
+                    if (qDmg > tw.Health)
+                    {
+                        Utility.DrawCircle(ObjectManager.Player.ServerPosition, Q.Range, System.Drawing.Color.Red);
+                        Drawing.DrawText(Drawing.Width * 0.1f, Drawing.Height * 0.4f, System.Drawing.Color.Red, "W can kill: " + t.ChampionName + " have: " + t.Health + "hp");
+                    }
+                }
+            }
+        }
+    }
+}
--- Graves_OnKeyToWin/Graves_OnKeyToWin/Properties/AssemblyInfo.cs
+++ Graves_OnKeyToWin/Graves_OnKeyToWin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jinx_OneKeyToWin")]
+[assembly: AssemblyTitle("Graves_OnKeyToWin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Jinx_OneKeyToWin")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Graves_OnKeyToWin")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("632c037a-842b-4e8c-839d-4cc3c3e6115b")]
+[assembly: Guid("93fa15f4-1340-4d9d-b7d8-2fa48f5e70fe")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyFileVersion("1.9.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
