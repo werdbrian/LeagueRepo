de523659c11c57dae4f849ea6ee6cf8b2d355159
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-07-12 23:00:00 +0200

    Ekko rework

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Ekko.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Ekko.cs
@@ -12,23 +12,22 @@ namespace OneKeyToWin_AIO_Sebby
 {
     class Ekko
     {
-
         private Menu Config = Program.Config;
         public static Orbwalking.Orbwalker Orbwalker = Program.Orbwalker;
 
         private Spell E, Q, Q1,  R, W;
         private Obj_AI_Hero Player { get { return ObjectManager.Player; } }
-        private float QMANA, WMANA, EMANA, RMANA;
-        private static GameObject RMissile, WMissile;
+        private float QMANA, WMANA, EMANA, RMANA, Wtime = 0, Wtime2 = 0;
+        private static GameObject RMissile, WMissile2, WMissile, QMissile = null;
         public void LoadOKTW()
         {
-            Q = new Spell(SpellSlot.Q, 700); 
+            Q = new Spell(SpellSlot.Q, 750); 
             Q1 = new Spell(SpellSlot.Q, 1000);
-            W = new Spell(SpellSlot.W, 1400);
+            W = new Spell(SpellSlot.W, 1620);
             E = new Spell(SpellSlot.E, 330f);
-            R = new Spell(SpellSlot.R, 1200f);
+            R = new Spell(SpellSlot.R, 375f);
 
-            Q.SetSkillshot(0.25f, 50f, 2000f, false, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 60f, 2200f, false, SkillshotType.SkillshotLine);
             Q1.SetSkillshot(0.5f, 150f, 1000f, false, SkillshotType.SkillshotCircle);
             W.SetSkillshot(2.5f, 200f, float.MaxValue, false, SkillshotType.SkillshotCircle);
             R.SetSkillshot(0.6f, 375f, float.MaxValue, false, SkillshotType.SkillshotCircle);
@@ -37,18 +36,46 @@ namespace OneKeyToWin_AIO_Sebby
             Config.SubMenu("Draw").AddItem(new MenuItem("wRange", "W range").SetValue(false));
             Config.SubMenu("Draw").AddItem(new MenuItem("eRange", "E range").SetValue(false));
             Config.SubMenu("Draw").AddItem(new MenuItem("rRange", "R range").SetValue(false));
+            Config.SubMenu("Draw").AddItem(new MenuItem("Qhelp", "Show Q,W helper").SetValue(true));
             Config.SubMenu("Draw").AddItem(new MenuItem("onlyRdy", "Draw only ready spells").SetValue(true));
             foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(enemy => enemy.Team != Player.Team))
                 Config.SubMenu(Player.ChampionName).SubMenu("Haras Q").AddItem(new MenuItem("haras" + enemy.ChampionName, enemy.ChampionName).SetValue(true));
             Config.SubMenu(Player.ChampionName).SubMenu("Farm").AddItem(new MenuItem("farmQ", "Lane clear Q").SetValue(true));
             Config.SubMenu(Player.ChampionName).SubMenu("Farm").AddItem(new MenuItem("Mana", "LaneClear Mana").SetValue(new Slider(80, 100, 30)));
             Config.SubMenu(Player.ChampionName).SubMenu("Farm").AddItem(new MenuItem("farmW", "Farm W").SetValue(true));
-            Config.SubMenu(Player.ChampionName).AddItem(new MenuItem("autoR", "Auto R").SetValue(true));
-            Config.SubMenu(Player.ChampionName).AddItem(new MenuItem("Rdmg", "R dmg % hp").SetValue(new Slider(20, 100, 0)));
+            Config.SubMenu(Player.ChampionName).SubMenu("R option").AddItem(new MenuItem("autoR", "Auto R").SetValue(true));
+            Config.SubMenu(Player.ChampionName).SubMenu("R option").AddItem(new MenuItem("Rdmg", "R dmg % hp").SetValue(new Slider(20, 100, 0)));
+            Config.SubMenu(Player.ChampionName).SubMenu("R option").AddItem(new MenuItem("rCount", "Auto R if enemies in range").SetValue(new Slider(3, 0, 5)));
 
             Game.OnUpdate += Game_OnGameUpdate;
             Obj_AI_Base.OnCreate += Obj_AI_Base_OnCreate;
             Drawing.OnDraw += Drawing_OnDraw;
+            Obj_SpellMissile.OnCreate += SpellMissile_OnCreateOld;
+            Obj_SpellMissile.OnDelete += Obj_SpellMissile_OnDelete;
+        }
+
+        private void Obj_SpellMissile_OnDelete(GameObject sender, EventArgs args)
+        {
+            if (!sender.IsValid<MissileClient>())
+                return;
+            MissileClient missile = (MissileClient)sender;
+            
+            if (missile.IsValid && missile.IsAlly && missile.SData.Name != null && (missile.SData.Name == "ekkoqmis" || missile.SData.Name == "ekkoqreturn"))
+            {
+                QMissile = null;
+            }
+        }
+
+        private void SpellMissile_OnCreateOld(GameObject sender, EventArgs args)
+        {
+            if (!sender.IsValid<MissileClient>())
+                return;
+            MissileClient missile = (MissileClient)sender;
+            
+            if (missile.IsValid && missile.IsAlly && missile.SData.Name != null && (missile.SData.Name == "ekkoqmis" || missile.SData.Name == "ekkoqreturn"))
+            {
+                QMissile = sender;
+            }
         }
 
         private void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
@@ -80,7 +107,6 @@ namespace OneKeyToWin_AIO_Sebby
                 LogicW();
             if (Program.LagFree(3) && E.IsReady() )
                 LogicE();
-            
             if (Program.LagFree(4) && R.IsReady() )
                 LogicR();
         }
@@ -91,18 +117,13 @@ namespace OneKeyToWin_AIO_Sebby
             {
                 foreach (var t in Program.Enemies.Where(t =>RMissile != null && RMissile.IsValid && t.IsValidTarget() && RMissile.Position.Distance(Prediction.GetPrediction(t, R.Delay).CastPosition) < 350 && RMissile.Position.Distance(t.ServerPosition) < 350))
                 {
+                    var comboDmg = R.GetDamage(t) + GetWdmg(t) + Q.GetDamage(t) * 2 + E.GetDamage(t);
 
-                    var comboDmg = GetRdmg(t) + GetWdmg(t);
-                    if (Q.IsReady())
-                        comboDmg += GetQdmg(t);
-                    if (E.IsReady())
-                        comboDmg += GetEdmg(t);
                     if (t.Health < comboDmg)
                         R.Cast();
                     Program.debug("ks");
-
-
-
+                    if (RMissile.Position.CountEnemiesInRange(R.Range) >= Config.Item("rCount").GetValue<Slider>().Value && Config.Item("rCount").GetValue<Slider>().Value > 0)
+                        R.Cast();
                 }
 
                 if (Player.Health < Player.CountEnemiesInRange(600) * Player.Level * 15)
@@ -122,7 +143,7 @@ namespace OneKeyToWin_AIO_Sebby
                 }
             }
 
-            var t = TargetSelector.GetTarget(900, TargetSelector.DamageType.Magical);
+            var t = TargetSelector.GetTarget(800, TargetSelector.DamageType.Magical);
 
             if (E.IsReady() && ObjectManager.Player.Mana > RMANA + EMANA
                  && ObjectManager.Player.CountEnemiesInRange(260) > 0
@@ -148,12 +169,20 @@ namespace OneKeyToWin_AIO_Sebby
         }
         private void Obj_AI_Base_OnCreate(GameObject obj, EventArgs args)
         {
-            if (obj.IsValid && obj.IsAlly )
+            if (obj.IsValid  )
             {
-                if (obj.Name == "Ekko")
+                if (obj.Name == "Ekko" && obj.IsAlly)
                     RMissile = obj;
-                if (obj.Name == "Ekko_Base_W_Cas.troy")
+                if (obj.Name == "Ekko_Base_W_Indicator.troy")
+                {
                     WMissile = obj;
+                    Wtime = Game.Time;
+                }
+                if (obj.Name == "Ekko_Base_W_Cas.troy")
+                {
+                    WMissile2 = obj;
+                    Wtime2 = Game.Time;
+                }
             }     
         }
 
@@ -163,7 +192,8 @@ namespace OneKeyToWin_AIO_Sebby
             var t1 = TargetSelector.GetTarget(Q1.Range, TargetSelector.DamageType.Physical);
             if (t.IsValidTarget())
             {
-                Program.debug("" + GetQdmg(t));
+
+                Program.debug("" + Q.GetDamage(t));
                 var qDmg = GetQdmg(t);
                 if (qDmg > t.Health)
                     Q.Cast(t, true);
@@ -207,7 +237,7 @@ namespace OneKeyToWin_AIO_Sebby
         private void LogicW()
         {
             var t = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Physical);
-            if (t.IsValidTarget())
+            if (t.IsValidTarget() && t.IsMoving)
             {
                 var qDmg = GetQdmg(t);
                 if (t.HasBuffOfType(BuffType.Slow) || t.CountEnemiesInRange(250) > 1)
@@ -275,8 +305,40 @@ namespace OneKeyToWin_AIO_Sebby
             double dmg = 50 + (150 * R.Level) + Player.FlatMagicDamageMod * 1.3;
             return Player.CalcDamage(t, Damage.DamageType.Magical, dmg);
         }
+
+        public static void drawLine(Vector3 pos1, Vector3 pos2, int bold, System.Drawing.Color color)
+        {
+            var wts1 = Drawing.WorldToScreen(pos1);
+            var wts2 = Drawing.WorldToScreen(pos2);
+
+            Drawing.DrawLine(wts1[0], wts1[1], wts2[0], wts2[1], bold, color);
+        }
+
+        public static void drawText2(string msg, Vector3 Hero, System.Drawing.Color color)
+        {
+            var wts = Drawing.WorldToScreen(Hero);
+            Drawing.DrawText(wts[0] - (msg.Length) * 5, wts[1] - 200, color, msg);
+        }
+
         private void Drawing_OnDraw(EventArgs args)
         {
+            if (QMissile != null && Config.Item("Qhelp").GetValue<bool>())
+            {
+                OktwCommon.DrawLineRectangle(QMissile.Position, Player.Position, (int)Q.Width, 1, System.Drawing.Color.White);
+
+                if (WMissile != null && WMissile.IsValid)
+                {
+                    Utility.DrawCircle(WMissile.Position, 300, System.Drawing.Color.Yellow, 1, 1);
+                    drawText2("W:  " + String.Format("{0:0.0}", Wtime + 3 - Game.Time), WMissile.Position, System.Drawing.Color.White);
+
+                }
+                if (WMissile2 != null && WMissile2.IsValid)
+                {
+                    Utility.DrawCircle(WMissile2.Position, 300, System.Drawing.Color.Red, 1, 1);
+                    drawText2("W:  " + String.Format("{0:0.0}", Wtime2 + 1 - Game.Time), WMissile2.Position, System.Drawing.Color.Red);
+
+                }
+            }
             if (Config.Item("watermark").GetValue<bool>())
             {
                 Drawing.DrawText(Drawing.Width * 0.2f, Drawing.Height * 0f, System.Drawing.Color.Cyan, "OneKeyToWin AIO - " + Player.ChampionName + " by Sebby");
@@ -301,31 +363,33 @@ namespace OneKeyToWin_AIO_Sebby
                 }
                 else
                     Utility.DrawCircle(ObjectManager.Player.Position, W.Range, System.Drawing.Color.Orange, 1, 1);
+                
             }
             if (Config.Item("eRange").GetValue<bool>())
             {
                 if (Config.Item("onlyRdy").GetValue<bool>())
                 {
                     if (E.IsReady())
-                        Utility.DrawCircle(ObjectManager.Player.Position, 925, System.Drawing.Color.Yellow, 1, 1);
+                        Utility.DrawCircle(ObjectManager.Player.Position, 800, System.Drawing.Color.Yellow, 1, 1);
                 }
                 else
-                    Utility.DrawCircle(ObjectManager.Player.Position, 925, System.Drawing.Color.Yellow, 1, 1);
+                    Utility.DrawCircle(ObjectManager.Player.Position, 800, System.Drawing.Color.Yellow, 1, 1);
             }
             if (Config.Item("rRange").GetValue<bool>())
             {
                 if (RMissile != null && RMissile.IsValid)
                 {
-
                     if (Config.Item("rRange").GetValue<bool>())
                     {
                         if (Config.Item("onlyRdy").GetValue<bool>())
                         {
                             if (R.IsReady())
-                                Utility.DrawCircle(RMissile.Position, R.Width, System.Drawing.Color.Gray, 1, 1);
+                                Utility.DrawCircle(RMissile.Position, R.Width, System.Drawing.Color.YellowGreen, 1, 1);
                         }
                         else
-                            Utility.DrawCircle(RMissile.Position, R.Width, System.Drawing.Color.Gray, 1, 1);
+                            Utility.DrawCircle(RMissile.Position, R.Width, System.Drawing.Color.YellowGreen, 1, 1);
+
+                        drawLine(RMissile.Position, Player.Position, 10, System.Drawing.Color.YellowGreen);
                     }
                 }
             }
