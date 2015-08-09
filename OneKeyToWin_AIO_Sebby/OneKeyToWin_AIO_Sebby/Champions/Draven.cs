ac393c614a6238a0f10a0c1fb091dcb1f2cf18d3
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-08-09 10:16:27 +0200

    Draven beta test

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Draven.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Draven.cs
@@ -17,17 +17,19 @@ namespace OneKeyToWin_AIO_Sebby.Champions
         private float QMANA, WMANA, EMANA, RMANA;
         private int axeCatchRange;
         public Obj_AI_Hero Player { get { return ObjectManager.Player; } }
-       
+        private static GameObject RMissile = null;
         public List<GameObject> axeList = new List<GameObject>();
 
         public void LoadOKTW()
         {
+            
             Q = new Spell(SpellSlot.Q);
             W = new Spell(SpellSlot.W);
             E = new Spell(SpellSlot.E, 1100);
+            R = new Spell(SpellSlot.E, 3000);
 
             E.SetSkillshot(0.25f, 130, 1400, false, SkillshotType.SkillshotLine);
-            R.SetSkillshot(0.4f, 160, 2000, true, SkillshotType.SkillshotLine);
+            R.SetSkillshot(0.4f, 160, 2000, false, SkillshotType.SkillshotLine);
 
             Config.SubMenu(Player.ChampionName).SubMenu("AXE option").AddItem(new MenuItem("axeCatchRange", "Axe catch range").SetValue(new Slider(500, 200, 2000)));
 
@@ -43,10 +45,59 @@ namespace OneKeyToWin_AIO_Sebby.Champions
             Config.SubMenu(Player.ChampionName).SubMenu("R config").AddItem(new MenuItem("hitchanceR", "VeryHighHitChanceR").SetValue(true));
             Config.SubMenu(Player.ChampionName).SubMenu("R config").AddItem(new MenuItem("useR", "Semi-manual cast R key").SetValue(new KeyBind('t', KeyBindType.Press))); //32 == space
 
+            Obj_SpellMissile.OnCreate += SpellMissile_OnCreateOld;
+            Obj_SpellMissile.OnDelete += Obj_SpellMissile_OnDelete;
+            Orbwalking.BeforeAttack += BeforeAttack;
             GameObject.OnCreate += GameObjectOnOnCreate;
             GameObject.OnDelete += GameObjectOnOnDelete;
             Drawing.OnDraw += Drawing_OnDraw;
             Game.OnUpdate += GameOnOnUpdate;
+            AntiGapcloser.OnEnemyGapcloser += AntiGapcloser_OnEnemyGapcloser;
+
+        }
+
+        private void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser)
+        {
+            
+        }
+
+        private void Obj_SpellMissile_OnDelete(GameObject sender, EventArgs args)
+        {
+            if (!sender.IsValid<MissileClient>())
+                return;
+            MissileClient missile = (MissileClient)sender;
+
+            if (missile.IsValid && missile.IsAlly && missile.SData.Name != null && missile.SData.Name == "DravenR")
+            {
+                RMissile = null;
+            }
+        }
+
+        private void SpellMissile_OnCreateOld(GameObject sender, EventArgs args)
+        {
+            if (!sender.IsValid<MissileClient>())
+                return;
+
+            MissileClient missile = (MissileClient)sender;
+
+            if (missile.IsValid && missile.IsAlly && missile.SData.Name != null && missile.SData.Name == "DravenR")
+            {
+                RMissile = sender;
+            }
+        }
+
+        private void BeforeAttack(Orbwalking.BeforeAttackEventArgs args)
+        {
+            //Program.debug("" + OktwCommon.GetBuffCount(Player, "dravenspinningattack"));
+            if (Q.IsReady())
+            {
+                if (Program.Combo && Player.Mana > RMANA + QMANA && OktwCommon.GetBuffCount(Player,"dravenspinningattack") + axeList.Count < 2)
+                {
+                    Q.Cast();
+                }
+                if (Program.Farm && Player.Mana > RMANA + QMANA + EMANA + WMANA && OktwCommon.GetBuffCount(Player, "dravenspinningattack") + axeList.Count == 0)
+                    Q.Cast();
+            }
         }
 
         private void GameObjectOnOnCreate(GameObject sender, EventArgs args)
@@ -68,15 +119,75 @@ namespace OneKeyToWin_AIO_Sebby.Champions
 
         private void GameOnOnUpdate(EventArgs args)
         {
+            SetMana();
+            //Program.debug("" + OktwCommon.GetBuffCount(Player, "dravenspinningattack"));
             axeCatchRange = Config.Item("axeCatchRange").GetValue<Slider>().Value;
             axeList.RemoveAll(x => !x.IsValid);
             AxeLogic();
+
+            if (Program.LagFree(1) && E.IsReady() && !Player.IsWindingUp)
+                LogicE();
+
+            if (Program.LagFree(3) && W.IsReady() && !Player.HasBuff("dravenfurybuff"))
+                LogicW();
+
+            if (Program.LagFree(4) && R.IsReady() && !Player.IsWindingUp)
+                LogicR();
+        }
+
+        private void LogicW()
+        {
+            if (Program.Combo && Player.Mana > RMANA + EMANA + WMANA && Player.CountEnemiesInRange(1000) > 0)
+            {
+                W.Cast();
+            }
+        }
+
+        private void LogicE()
+        {
+            foreach (var enemy in Program.Enemies.Where(enemy => enemy.IsValidTarget(E.Range) && !Orbwalking.InAutoAttackRange(enemy) && E.GetDamage(enemy) > enemy.Health))
+            {
+                Program.CastSpell(E, enemy);
+                return;
+            }
+
+            var t = TargetSelector.GetTarget(E.Range, TargetSelector.DamageType.Physical);
+            if (t.IsValidTarget())
+            {
+                if (Program.Combo && Player.Mana > RMANA + EMANA)
+                {
+                    if (!Orbwalking.InAutoAttackRange(t))
+                    {
+                        Program.CastSpell(E, t);
+                    }
+                    R.CastIfWillHit(t, 2, true);
+                    if(Player.Health < Player.MaxHealth * 0.5)
+                        Program.CastSpell(E, t);
+
+                }
+            }
+            foreach (var target in Program.Enemies.Where(target => target.IsValidTarget(E.Range)))
+            {
+                if (target.IsValidTarget(300) && target.IsMelee)
+                {
+                    Program.CastSpell(E, t);
+                }
+            }
         }
 
         private void LogicR()
         {
+            if (Config.Item("useR").GetValue<KeyBind>().Active)
+            {
+                var t = TargetSelector.GetTarget(R.Range, TargetSelector.DamageType.Physical);
 
-            if (Config.Item("autoR").GetValue<bool>() && Player.CountEnemiesInRange(800) == 0 )
+                if (t.IsValidTarget())
+                {
+                    R.CastIfWillHit(t, 2, true);
+                    R.Cast(t, true, true);
+                }
+            }
+            if (Config.Item("autoR").GetValue<bool>())
             {
                 foreach (var target in Program.Enemies.Where(target => target.IsValidTarget(R.Range) && Program.ValidUlt(target)))
                 {
@@ -111,10 +222,10 @@ namespace OneKeyToWin_AIO_Sebby.Champions
                     {
                         R.CastIfWillHit(target, 2, true);
                     }
-
                 }
             }
         }
+
         private void castR(Obj_AI_Hero target)
         {
             if (Config.Item("hitchanceR").GetValue<bool>())
@@ -126,11 +237,12 @@ namespace OneKeyToWin_AIO_Sebby.Champions
                 }
             }
             else
-                R.Cast(target, true);
+                Program.CastSpell(R, target);
         }
+
         private double getRdmg(Obj_AI_Base target)
         {
-            var rDmg = R.GetDamage(target) * 2;
+            var rDmg = R.GetDamage(target) ;
             var dmg = 0;
             PredictionOutput output = R.GetPrediction(target);
             Vector2 direction = output.CastPosition.To2D() - Player.Position.To2D();
@@ -169,9 +281,9 @@ namespace OneKeyToWin_AIO_Sebby.Champions
             //    Game.PrintChat("R collision" + dmg);
 
             if (dmg > 7)
-                return rDmg * 0.7;
+                return rDmg * 0.7 + R.GetDamage(target);
             else
-                return rDmg - (rDmg * 0.1 * dmg);
+                return rDmg - (rDmg * 0.1 * dmg) + R.GetDamage(target);
         }
 
         private void AxeLogic()
@@ -201,13 +313,32 @@ namespace OneKeyToWin_AIO_Sebby.Champions
 
         private void CatchAxe(GameObject Axe)
         {
-            if (Player.Distance(Axe.Position) > 120 && Game.CursorPos.Distance(Axe.Position) < axeCatchRange)
+            if (Player.Distance(Axe.Position) > 120 && Game.CursorPos.Distance(Axe.Position) < axeCatchRange && !Axe.Position.UnderTurret(true))
             {
                 Orbwalker.SetOrbwalkingPoint(Axe.Position);
             }
             else
             {
                 Orbwalker.SetOrbwalkingPoint(Game.CursorPos);
+            }
+        }
+
+        private void SetMana()
+        {
+            QMANA = Q.Instance.ManaCost;
+            WMANA = W.Instance.ManaCost;
+            EMANA = E.Instance.ManaCost;
+            if (!R.IsReady())
+                RMANA = EMANA - Player.PARRegenRate * E.Instance.Cooldown;
+            else
+                RMANA = R.Instance.ManaCost;
+
+            if (Player.Health < Player.MaxHealth * 0.2)
+            {
+                QMANA = 0;
+                WMANA = 0;
+                EMANA = 0;
+                RMANA = 0;
             }
         }
 
@@ -219,6 +350,8 @@ namespace OneKeyToWin_AIO_Sebby.Champions
             }
 
             Utility.DrawCircle(Game.CursorPos, axeCatchRange, System.Drawing.Color.LemonChiffon, 1, 1);
+            if (RMissile != null )
+                OktwCommon.DrawLineRectangle(RMissile.Position, Player.Position, (int)R.Width, 1, System.Drawing.Color.White);
         }
     }
 }
