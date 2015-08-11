1809f0d1d695523d757b8d11f661dfde1abb247e
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-08-11 10:52:28 +0200

    Thresh E options

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Thresh.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Thresh.cs
@@ -50,6 +50,7 @@ namespace OneKeyToWin_AIO_Sebby.Champions
             Config.SubMenu(Player.ChampionName).SubMenu("W option").AddItem(new MenuItem("wCount", "Auto W if x enemies near ally").SetValue(new Slider(3, 0, 5)));
 
             Config.SubMenu(Player.ChampionName).SubMenu("E option").AddItem(new MenuItem("autoE", "Auto E").SetValue(true));
+            Config.SubMenu(Player.ChampionName).SubMenu("E option").AddItem(new MenuItem("pushE", "Auto push").SetValue(true));
             Config.SubMenu(Player.ChampionName).SubMenu("E option").AddItem(new MenuItem("inter", "OnPossibleToInterrupt")).SetValue(true);
             Config.SubMenu(Player.ChampionName).SubMenu("E option").AddItem(new MenuItem("Gap", "OnEnemyGapcloser")).SetValue(true);
 
@@ -120,9 +121,9 @@ namespace OneKeyToWin_AIO_Sebby.Champions
         {
             if (Program.LagFree(1) && Q.IsReady())
                 LogicQ();
-            if (Program.LagFree(2) && E.IsReady())
+            if (Program.LagFree(2) && E.IsReady() && Config.Item("autoE").GetValue<bool>())
                 LogicE();
-            if (Program.LagFree(3) && W.IsReady() )
+            if (Program.LagFree(3) && W.IsReady())
                 LogicW();
             if (Program.LagFree(4) && R.IsReady())
                 LogicR();
@@ -143,7 +144,7 @@ namespace OneKeyToWin_AIO_Sebby.Champions
                 {
                     CastE(false, t);
                 }
-                else
+                else if (Config.Item("pushE").GetValue<bool>())
                 {
                     CastE(true, t);
                 }
@@ -155,7 +156,6 @@ namespace OneKeyToWin_AIO_Sebby.Champions
         {
             foreach (var enemy in Program.Enemies.Where(enemy => enemy.IsValidTarget(Q.Range) && enemy.HasBuff("ThreshQ")))
             {
-
                 if (Program.Combo)
                 {
                     if (W.IsReady() && Config.Item("autoW2").GetValue<bool>())
