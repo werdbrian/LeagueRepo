1c127cac9e785cf8375a1a4a7d32042bf5b1fcc9
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-08-14 11:18:07 +0200

    Brain new thread

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/OneKeyToBrain.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/OneKeyToBrain.cs
@@ -56,7 +56,7 @@ namespace OneKeyToWin_AIO_Sebby.Core
 
         private void OnUpdate(EventArgs args)
         {
-            if (!Program.LagFree(4))
+            if (!Program.LagFree(3))
                 return;
             foreach (var enemy in Program.Enemies.Where(enemy => enemy.IsValid))
             {
