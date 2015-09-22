d93161c060da4a356e7243311a040cb8f00b7ca3
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-09-22 20:03:30 +0200

    Prediction improve fps

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/PredictionOktw.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/PredictionOktw.cs
@@ -336,11 +336,21 @@ namespace OneKeyToWin_AIO_Sebby.Core
             //Check for collision
             if (checkCollision && input.Collision && result.Hitchance > HitChance.Impossible)
             {
-                var positions = new List<Vector3> { result.CastPosition , input.Unit.ServerPosition};
-
-                result.CollisionObjects = Collision.GetCollision(positions, input);
+                result.CollisionObjects = Collision.GetCollision(result.CastPosition, input);
                 result.CollisionObjects.RemoveAll(x => x.NetworkId == input.Unit.NetworkId);
-                result.Hitchance = result.CollisionObjects.Count > 0 ? HitChance.Collision : result.Hitchance;
+
+                if (result.CollisionObjects.Count > 0)
+                    result.Hitchance = HitChance.Collision;
+                else
+                {
+                    result.CollisionObjects = Collision.GetCollision(input.Unit.ServerPosition, input);
+                    result.CollisionObjects.RemoveAll(x => x.NetworkId == input.Unit.NetworkId);
+
+                    if (result.CollisionObjects.Count > 0)
+                        result.Hitchance = HitChance.Collision;
+                }
+
+                
             }
             //Set hit chance
             if (result.Hitchance == HitChance.High || result.Hitchance == HitChance.VeryHigh)
@@ -1013,122 +1023,118 @@ namespace OneKeyToWin_AIO_Sebby.Core
         /// <summary>
         ///     Returns the list of the units that the skillshot will hit before reaching the set positions.
         /// </summary>
-        public static List<Obj_AI_Base> GetCollision(List<Vector3> positions, PredictionInput input)
+        public static List<Obj_AI_Base> GetCollision(Vector3 position, PredictionInput input)
         {
             var result = new List<Obj_AI_Base>();
 
-            foreach (var position in positions)
+            foreach (var objectType in input.CollisionObjects)
             {
-                foreach (var objectType in input.CollisionObjects)
+                switch (objectType)
                 {
-                    switch (objectType)
-                    {
-                        case CollisionableObjects.Minions:
-                            foreach (var minion in ObjectManager.Get<Obj_AI_Minion>().Where(minion =>
-                                            minion.IsValidTarget(Math.Min(input.Range + input.Radius + 100, 2000), true, input.From)))
+                    case CollisionableObjects.Minions:
+                        foreach (var minion in ObjectManager.Get<Obj_AI_Minion>().Where(minion =>
+                                        minion.IsValidTarget(Math.Min(input.Range + input.Radius + 100, 2000), true, input.From)))
+                        {
+                            input.Unit = minion;
+                            if (minion.Path.Count() > 0)
                             {
-                                input.Unit = minion;
-                                if (minion.Path.Count() > 0)
-                                {
-                                    var minionPrediction = Prediction.GetPrediction(input, true, false);
+                                var minionPrediction = Prediction.GetPrediction(input, true, false);
 
-                                    if (minionPrediction.CastPosition.To2D().Distance(input.From.To2D(), position.To2D(), true, true) <= Math.Pow((input.Radius + 20 + minion.Path.Count() * minion.BoundingRadius), 2))
-                                    {
-                                        result.Add(minion);
-                                    }
-                                }
-                                else
+                                if (minionPrediction.CastPosition.To2D().Distance(input.From.To2D(), position.To2D(), true, true) <= Math.Pow((input.Radius + 20 + minion.Path.Count() * minion.BoundingRadius), 2))
                                 {
-                                    var bonus = 20;
-                                    if (minion.ServerPosition.To2D().Distance(input.From.To2D()) < input.Radius/2)
-                                        bonus = 40;
-                                    if (minion.ServerPosition.To2D().Distance(input.From.To2D(), position.To2D(), true, true) <=
-                                        Math.Pow((input.Radius + bonus + minion.BoundingRadius), 2))
-                                    {
-                                        result.Add(minion);
-                                    }
+                                    result.Add(minion);
                                 }
                             }
+                            else
+                            {
+                                var bonus = 20;
+                                if (minion.ServerPosition.To2D().Distance(input.From.To2D()) < input.Radius/2)
+                                    bonus = 40;
+                                if (minion.ServerPosition.To2D().Distance(input.From.To2D(), position.To2D(), true, true) <=
+                                    Math.Pow((input.Radius + bonus + minion.BoundingRadius), 2))
+                                {
+                                    result.Add(minion);
+                                }
+                            }
+                        }
+                        break;
+                    case CollisionableObjects.Heroes:
+                        foreach (var hero in
+                            HeroManager.Enemies.FindAll(
+                                hero =>
+                                    hero.IsValidTarget(
+                                        Math.Min(input.Range + input.Radius + 100, 2000), true, input.RangeCheckFrom))
+                            )
+                        {
+                            input.Unit = hero;
+                            var prediction = Prediction.GetPrediction(input, false, false);
+                            if (
+                                prediction.UnitPosition.To2D()
+                                    .Distance(input.From.To2D(), position.To2D(), true, true) <=
+                                Math.Pow((input.Radius + 50 + hero.BoundingRadius), 2))
+                            {
+                                result.Add(hero);
+                            }
+                        }
+                        break;
+
+                    case CollisionableObjects.Walls:
+                        var step = position.Distance(input.From) / 20;
+                        for (var i = 0; i < 20; i++)
+                        {
+                            var p = input.From.To2D().Extend(position.To2D(), step * i);
+                            if (NavMesh.GetCollisionFlags(p.X, p.Y).HasFlag(CollisionFlags.Wall))
+                            {
+                                result.Add(ObjectManager.Player);
+                            }
+                        }
+                        break;
+
+                    case CollisionableObjects.YasuoWall:
+
+                        if (Utils.TickCount - _wallCastT > 4000)
+                        {
                             break;
-                        case CollisionableObjects.Heroes:
-                            foreach (var hero in
-                                HeroManager.Enemies.FindAll(
-                                    hero =>
-                                        hero.IsValidTarget(
-                                            Math.Min(input.Range + input.Radius + 100, 2000), true, input.RangeCheckFrom))
-                                )
-                            {
-                                input.Unit = hero;
-                                var prediction = Prediction.GetPrediction(input, false, false);
-                                if (
-                                    prediction.UnitPosition.To2D()
-                                        .Distance(input.From.To2D(), position.To2D(), true, true) <=
-                                    Math.Pow((input.Radius + 50 + hero.BoundingRadius), 2))
-                                {
-                                    result.Add(hero);
-                                }
-                            }
+                        }
+
+                        GameObject wall = null;
+                        foreach (var gameObject in
+                            ObjectManager.Get<GameObject>()
+                                .Where(
+                                    gameObject =>
+                                        gameObject.IsValid &&
+                                        Regex.IsMatch(
+                                            gameObject.Name, "_w_windwall_enemy_0.\\.troy", RegexOptions.IgnoreCase))
+                            )
+                        {
+                            wall = gameObject;
+                        }
+                        if (wall == null)
+                        {
                             break;
+                        }
+                        var level = wall.Name.Substring(wall.Name.Length - 6, 1);
+                        var wallWidth = (300 + 50 * Convert.ToInt32(level));
 
-                        case CollisionableObjects.Walls:
-                            var step = position.Distance(input.From) / 20;
-                            for (var i = 0; i < 20; i++)
+                        var wallDirection =
+                            (wall.Position.To2D() - _yasuoWallCastedPos).Normalized().Perpendicular();
+                        var wallStart = wall.Position.To2D() + wallWidth / 2f * wallDirection;
+                        var wallEnd = wallStart - wallWidth * wallDirection;
+
+                        if (wallStart.Intersection(wallEnd, position.To2D(), input.From.To2D()).Intersects)
+                        {
+                            var t = Utils.TickCount +
+                                    (wallStart.Intersection(wallEnd, position.To2D(), input.From.To2D())
+                                        .Point.Distance(input.From) / input.Speed + input.Delay) * 1000;
+                            if (t < _wallCastT + 4000)
                             {
-                                var p = input.From.To2D().Extend(position.To2D(), step * i);
-                                if (NavMesh.GetCollisionFlags(p.X, p.Y).HasFlag(CollisionFlags.Wall))
-                                {
-                                    result.Add(ObjectManager.Player);
-                                }
+                                result.Add(ObjectManager.Player);
                             }
-                            break;
+                        }
 
-                        case CollisionableObjects.YasuoWall:
-
-                            if (Utils.TickCount - _wallCastT > 4000)
-                            {
-                                break;
-                            }
-
-                            GameObject wall = null;
-                            foreach (var gameObject in
-                                ObjectManager.Get<GameObject>()
-                                    .Where(
-                                        gameObject =>
-                                            gameObject.IsValid &&
-                                            Regex.IsMatch(
-                                                gameObject.Name, "_w_windwall_enemy_0.\\.troy", RegexOptions.IgnoreCase))
-                                )
-                            {
-                                wall = gameObject;
-                            }
-                            if (wall == null)
-                            {
-                                break;
-                            }
-                            var level = wall.Name.Substring(wall.Name.Length - 6, 1);
-                            var wallWidth = (300 + 50 * Convert.ToInt32(level));
-
-                            var wallDirection =
-                                (wall.Position.To2D() - _yasuoWallCastedPos).Normalized().Perpendicular();
-                            var wallStart = wall.Position.To2D() + wallWidth / 2f * wallDirection;
-                            var wallEnd = wallStart - wallWidth * wallDirection;
-
-                            if (wallStart.Intersection(wallEnd, position.To2D(), input.From.To2D()).Intersects)
-                            {
-                                var t = Utils.TickCount +
-                                        (wallStart.Intersection(wallEnd, position.To2D(), input.From.To2D())
-                                            .Point.Distance(input.From) / input.Speed + input.Delay) * 1000;
-                                if (t < _wallCastT + 4000)
-                                {
-                                    result.Add(ObjectManager.Player);
-                                }
-                            }
-
-                            break;
-                    }
+                        break;
                 }
             }
-
             return result.Distinct().ToList();
         }
     }
